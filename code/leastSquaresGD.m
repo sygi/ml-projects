3d8b099b03f197e876d439f25f18ea343d71c6ac
Jakub Sygnowski <sygnowski@gmail.com>
2014-10-23 08:35:47 +0200

    test code

--- code/leastSquaresGD.m
+++ code/leastSquaresGD.m
@@ -11,10 +11,15 @@ if ~exist('maxIter', 'var')
     maxIter = 1000;
 end
 
+if ~exist('alpha', 'var')
+    alpha = 0.01;
+end
+
 [N,D] = size(tX);
 beta = zeros(D,1);
 for i = 1:maxIter
    gradient =  -(1/N)*tX'*(y - tX*beta);
+   e = (y - tX * beta);
    beta = beta - alpha*gradient;
 end
 end
