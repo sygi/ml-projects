0e7c7c06dc88540a5fda12125f38cc47389cbd98
Michalina Pacholska <micha7a@gmail.com>
2014-11-01 20:08:27 +0100

    brand new plots

--- code/somePlots.m
+++ code/somePlots.m
@@ -4,26 +4,24 @@ load('Toronto_classification');
 corX = corrcoef(X_train);
 figure(4);
 imagesc(corX);
-tit = title('corelation between input variables');
+tit = title('Corelation between input variables');
 preparePlot([tit]);
 print -dpdf imagesc_Xcorrelation.pdf;
 
 corY = corr(y_train,X_train);
 figure(3);
 bar(corY);
-hy = ylabel('correlation');
-hx = xlabel('numbeer of variable');
+hy = ylabel('correlation with $y$');
+hx = xlabel('input variable');
 preparePlot([hy,hx]);
 print -dpdf bar_ycorrelation.pdf;
 load('Toronto_classification');
 
-break;
-
 figure(1);
 boxplot(X_train, 'orientation', 'horizontal');
-hy = ylabel('number of variable ');
+hy = ylabel('input variable ');
 hx = xlabel('distribution of variable');
-tit = title('given data distribution');
+tit = title('distribution of given data');
 preparePlot([hx,hy,tit]);
 print -dpdf boxplot_before.pdf
 
@@ -32,8 +30,8 @@ clear all;
 load('classifNormalized');
 figure(2);
 boxplot(myX_train, 'orientation', 'horizontal');
-hy = ylabel('number of variable ');
+hy = ylabel('input variable');
 hx = xlabel('distribution of variable');
-tit = title('data distribution after preprocess');
+tit = title('distribution of data after preprocesing');
 preparePlot([hx,hy,tit]);
 print -dpdf boxplot_after.pdf
