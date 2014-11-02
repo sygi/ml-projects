98bb7c484a35881bb271a4d6dd2723b3ac85476b
Jakub Sygnowski <sygnowski@gmail.com>
2014-11-02 22:22:56 +0100

    submit folder

---
+++ submit/leastSquaresGD.m
@@ -0,0 +1,25 @@
+function beta = leastSquaresGD(y,tX,alpha,maxIter)
+%leastSquaresGD - least squares using gradient descent 
+%
+%   some comment
+%   
+%   beta = leastSquaresGD(y,tX,alpha)
+%   beta = leastSquaresGD(y,tX,alpha,maxIter)
+
+
+if ~exist('maxIter', 'var')
+    maxIter = 1000;
+end
+
+if ~exist('alpha', 'var')
+    alpha = 0.01;
+end
+
+[N,D] = size(tX);
+beta = zeros(D,1);
+for i = 1:maxIter
+   gradient =  -(1/N)*tX'*(y - tX*beta);
+   e = (y - tX * beta);
+   beta = beta - alpha*gradient;
+end
+end
