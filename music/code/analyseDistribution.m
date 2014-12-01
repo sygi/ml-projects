7fba8c6d256afcc0db179efcc33af556d51aef0b
Jakub Sygnowski <sygnowski@gmail.com>
2014-12-01 16:35:40 +0100

    first figures

--- music/code/analyseDistribution.m
+++ music/code/analyseDistribution.m
@@ -1,7 +1,5 @@
 artists = sum(Ytrain);
 artists = artists(artists > 0);
-% todo: podzielic ludzi na tych o count < 53 i tych, ktorzy maja wiecej i
-% im odjac
 
 subplot(2, 2, 1);
 hist(artists(artists < 10^3), 100);
@@ -25,4 +23,3 @@ subplot(2,2,4);
 hist(powerArtists, 100);
 t = title('$\frac{1}{\sqrt{\mbox{listen counts}}}$');
 preparePlot(t);
-
---
+++ music/code/analyseDistributionArtistSum.m
@@ -0,0 +1,27 @@
+data = Ytrain;
+data = data(find(data));
+
+hist(data(data < 10^3), 100);
+one = xlabel('listen count value');b
+two = ylabel('number of entries in $Y_{train}$');
+preparePlot([one two]);
+print -dpdf ../figures/listen_counts.pdf
+
+logArtists = log(data);
+hist(logArtists, 100);
+one = xlabel('logarithm of listen count value');
+two = ylabel('number of entries in $Y_{train}$');
+preparePlot([one two]);
+print -dpdf ../figures/log_listen_counts.pdf
+% 
+% sqrtArtists = sqrt(artists);
+% subplot(2,2,3);
+% hist(sqrtArtists(sqrtArtists < 100), 100);
+% t = title('$\sqrt{\mbox{listen counts}}$');
+% preparePlot(t);
+% 
+% powerArtists = data .^ (-0.5);
+% subplot(2,2,4);
+% hist(powerArtists, 100);
+% t = title('$\frac{1}{\sqrt{\mbox{listen counts}}}$');
+% preparePlot(t);
