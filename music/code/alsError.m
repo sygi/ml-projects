64575dd575a0e920e6b6ceb690d4de3ed4673522
Jakub Sygnowski <sygnowski@gmail.com>
2014-12-04 15:13:42 +0100

    als cd

--- music/code/alsError.m
+++ music/code/alsError.m
@@ -1,4 +1,9 @@
-function Err = alsError(A,U,T)
-
-
+function Err = alsError(A,U,Ytrain)
+    
+    Err = 0;
+    [id1,id2] = find(Ytrain);
+    for i=1:length(id1)
+       Err = Err + norm(A(:,id2(i))'*U(:,id1(i))-Ytrain(id1(i),id2(i)));
+    end
+    Err= Err/length(id1);
 end
