d772daef42732442d22bb52fe75ff3d51048a312
micha7a <micha7a@gmail.com>
2014-12-04 15:12:01 +0100

    example2

--- images/code/example.m
+++ images/code/example.m
@@ -22,19 +22,6 @@ addpath(genpath('../toolbox/'));
 %% --browse through the images, and show the feature visualization beside
 %  -- You should explore the features for the positive and negative
 %  examples and understand how they resemble the original image.
-for i=1:10
-    clf();
-    
-    subplot(121);
-    imshow(imgs{i}); % image itself
-    
-    subplot(122);
-    im( hogDraw(feats{i}) ); colormap gray;
-    axis off; colorbar off;
-    
-   % pause;  % wait for keydo that then, 
-end
-
 %% -- Generate feature vectors (so each one is a row of X)
 fprintf('Generating feature vectors..\n');
 D = numel(feats{1});  % feature dimensionality
@@ -47,17 +34,19 @@ end
 %% -- Example: split half and half into train/test
 fprintf('Splitting into train/test..\n');
 
-% NOTE: you should do k-fold!
-perm = randperm(size(X,1));
-split = 0.8;
-lastIdx = length(perm) * split;
-Tr.idxs = perm(1:lastIdx);
+K = 8;
+idx = randperm(size(X,1));
+err = zeros(K, 1);
+Nk = floor(length(idx)/K);
+for k=1:K
+Tr.idxs = idx(Nk+1:end);
 Tr.X = X(Tr.idxs,:);
 Tr.y = labels(Tr.idxs);
 
-Te.idxs = perm(lastIdx+1:end);
+Te.idxs = idx(1:Nk);
 Te.X = X(Te.idxs,:);
 Te.y = labels(Te.idxs);
+idx = circshift(idx, [0 Nk]);
 
 %%
 fprintf('Training simple neural network..\n');
@@ -75,18 +64,21 @@ addpath(genpath('where/the/deeplearningtoolboxis/'));
 %
 % Make sure you understand what is going on below!
 
-rng(8399);  % fix seed, this    NN is very sensitive to initialization
+seeds = randperm(10000);
+for seed=1:1
+rng(seeds(seed));  % fix seed, this    NN is very sensitive to initialization
 
 % setup NN. The first layer needs to have number of features neurons,
 %  and the last layer the number of classes (here two).
-nn = nnsetup([size(Tr.X,2) 50 2]);
-opts.numepochs =  25;   %  Numberdsaof full sweeps through data
+nn = nnsetup([size(Tr.X,2) 50 5 2]);
+opts.numepochs =  20;   %  Numberdsaof full sweeps through data
 opts.batchsize = 100;  %  Take a mean gradient step over this many samples
 
 % if == 1 => plots trainin error as the NN is trained
-opts.plot               = 1; 
+opts.plot = 0;
 
-nn.learningRate = 2;
+nn.activation_function = 'sigm';
+nn.learningRate = 1;
 
 % this neural network implementation requires number of samples to be a
 % multiple of batchsize, so we remove some for this to be true.
@@ -122,30 +114,15 @@ nnPred = nnPred(:,1) - nnPred(:,2);
 %% See prediction performance
 fprintf('Plotting performance..\n');
 % let's also see how random predicition does
-randPred = rand(size(Te.y));
+% randPred = rand(size(Te.y));
 
 % and plot all together, and get the performance of each
-methodNames = {'Neural Network', 'Random'}; % this is to show it in the legend
-avgTPRList = evaluateMultipleMethods( Te.y > 0, [nnPred,randPred], true, methodNames );
+%methodNames = {'Neural Network', 'Random'}; % this is to show it in the legend
+%avgTPRList = evaluateMultipleMethods( Te.y > 0, [nnPred,randPred], true, methodNames );
 
 % now you can see that the performance of each method
 % is in avgTPRList. You can see that random is doing very bad.
-avgTPRList
-
-%% visualize samples and their predictions (test set)
-figure;
-for i=20:30  % just 10 of them, though there are thousands
-    clf();
-    
-    subplot(121);
-    imshow(imgs{Te.idxs(i)}); % image itself
-    
-    subplot(122);
-    im( hogDraw(feats{Te.idxs(i)}) ); colormap gray;
-    axis off; colorbar off;
-    
-    % show if it is classified as pos or neg, and true label
-    title(sprintf('Label: %d, Pred: %d', labels(Te.idxs(i)), 2*(nnPred(i)>0) - 1));
-    
-    % pause;  % wait for keydo that then, 
+errTe(k) = fastROC(Te.y > 0, nnPred, false, '' )
+end
+mean(err)
 end
