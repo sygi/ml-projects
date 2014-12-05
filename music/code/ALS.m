c9bec12d95841ab7028cef708981633c34ea9745
Jakub Sygnowski <sygnowski@gmail.com>
2014-12-05 11:05:15 +0100

    als

--- music/code/ALS.m
+++ music/code/ALS.m
@@ -1,4 +1,4 @@
-function [A, U] = ALS(X, noHidden, lambda, maxIter)
+function [A, U, err] = ALS(X, noHidden, lambda, maxIter)
 %ALS compute A, U such that:
 % U' * A ~= X,
 % size(A) = [noHidden size(X, 2)]
@@ -19,13 +19,17 @@ U = rand(noHidden,usr);
 err = zeros(1,maxIter);
 
 for i=1:maxIter
+    if (mod(i, 10) == 0)
+        fprintf('ALS iteration %d\n', i);
+    end
 %    err(i)= rmse(A'*U,Ytrain;
 %    for j = 1:10
    [A, U] = alsUpdate(A, U, X, lambda);
 %    end
-   err(i) = alsError(A, U, X);
-   err(i)
-   if i>2
+   %err(i) = alsError(A, U, X);
+   if (mod(i, 50) > 1 && mod(i, 50) < 5)
+      err(i) = alsError(A, U, X);
+      err(i)
       if abs(err(i-1)-err(i)) < 1e-4;
           fprintf('converged');
           break;
