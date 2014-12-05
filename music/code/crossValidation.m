1e6edbab1700e2a1a5fb6aae9960040aef087a0c
Jakub Sygnowski <sygnowski@gmail.com>
2014-12-05 10:43:22 +0100

    cross validation

--- music/code/crossValidation.m
+++ music/code/crossValidation.m
@@ -1,25 +1,29 @@
-function [error, real] = crossValidation(train, social)
+function [error, real] = crossValidation(train, social, normalize)
 % running cross validation on some given algorithm
 % only weak test for now
-setSeed(4);
-p = linspace(0.01, 0.9, 1);
+if (exist('normalize', 'var') && normalize)
+    train = transformData(train);
+end
+p = randperm(100);
+p = p(1:10);
 real = zeros(length(p), 1);
 for current=1:length(p)
-    K = 5;
-    train_new = train;
-    [D N] = size(train_new);
+    setSeed(p(current));
+    K = current + 1;
+    [D N] = size(train);
     entries = length(find(train));
     perm = randperm(entries);
     idx = find(train);
     Nk = floor(entries / K);
     err = zeros(1, K);
+    fprintf('starting %d fold\n', K);
     for i = 1:K
         test_new = sparse([], [], [], D, N);
         test_new(idx(perm(1:Nk))) = train(idx(perm(1:Nk)));
         train_new = train;
         train_new(idx(perm(1:Nk))) = 0;
         % run some classifier
-        %err(i) = estimateMeanPrediction(train_new, test_new);
+        % err(i) = estimateMeanPrediction(train_new, test_new);
         %err(i) = p(current) * estimateNeighbourMean(train_new, social, test_new) ...
         %    + (1-p(current)) * estimateMeanPrediction(train_new, test_new);
         err(i) = estimateSlopeOne(train_new, test_new)
@@ -27,3 +31,4 @@ for current=1:length(p)
     end
     real(current) = mean(err)
 end
+error = mean(real);
